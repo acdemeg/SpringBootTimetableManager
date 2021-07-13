bb8416552e0817c585892ed71a5c94a6675bef59
acdemeg <v.krylov1996@gmail.com>
2021-07-13 21:56:09 +0600

    initialize project

---
+++ src/main/java/com/vkrylov/springboottimetable/SpringbootTimetableApplication.java
@@ -0,0 +1,13 @@
+package com.vkrylov.springboottimetable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTimetableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootTimetableApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vkrylov/springboottimetable/SpringbootTimetableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vkrylov.springboottimetable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTimetableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
